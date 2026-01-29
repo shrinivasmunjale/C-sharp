a22b07d012265127a8e7aaafbeb304e54e3cd3a2
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-01-29 22:39:51 +0530

    Initial commit

---
+++ User_details/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Student Name:");
+        string Name = Console.ReadLine();
+
+        Console.Write("Enter class: ");
+        string Class = Console.ReadLine();
+
+        Console.Write("Enter Phone number: ");
+        double num = Console.ReadLine();
+
+        Console.WriteLine("----------Student Details--------");
+        Console.WriteLine("Student Name: "+ Name);
+        Console.WriteLine("Student : "+ Name);
+
+
+
+    }
+}
