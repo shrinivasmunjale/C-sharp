53aa8237e169be57a4856b03986381599dee324a
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-01-29 23:06:01 +0530

    perform addition of two numbers

---
+++ add_Number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 =23;
+        int num2 = 22;
+
+        int result = num1+ num2;
+
+        Console.WriteLine("Addition of two number: "+result);
+    }
+}
