c8c64d9a63469d71e1fbb3e66bee3dcbfc3561c9
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-01-29 23:34:10 +0530

    some new program

---
+++ 4.find_max/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if(num1 > num2)
+        {
+            Console.WriteLine("Maximum number is :"+num1);
+        }
+        else
+        {
+            Console.WriteLine("Maximum number is :"+num2);
+        }
+
+    }
+}
---
+++ 5.evenOrOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      Console.Write("Enter number: ");
+      int num = Convert.ToInt32(Console.ReadLine()) ;
+
+      if(num % 2 == 0)
+        {
+            Console.Write("Given number is even");
+        }
+        else
+        {
+            Console.Write(num+" number is odd");
+        }
+
+    }
+}
