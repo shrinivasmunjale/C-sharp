f053baa9c33ffba296032f6249cdf1b07753df9d
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-02-03 22:58:19 +0530

    Initial commit

---
+++ 6.positiveOrNegative/Program.cs
@@ -0,0 +1,17 @@
+﻿using System ;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the any number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if(num > 0)
+        {
+            Console.WriteLine(num+" is positive number.");
+        }
+        else
+        {
+            Console.WriteLine(num+" is negative number.");
+        }
+    }
+}
---
+++ 7.simpleIntrest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the principle amount: ");
+        int p = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the Interest Rate: ");
+        int r = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the Time Peraid in year: ");
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        int result = (p*r*t)/100;
+
+        Console.WriteLine("Simple Interest is: "+ result);
+
+        int total = p + result ;
+
+        Console.WriteLine("Totle Amount including interest is: "+ total);
+
+    }
+}
