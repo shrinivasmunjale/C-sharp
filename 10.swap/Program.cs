1b5bbd31036c81687afebe4ed54d5ffee72b6581
Shrinivas Munjale <shrinivasmunjale3@gmail.com>
2026-02-04 10:46:36 +0530

    Add files via upload

---
+++ 10.swap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Buffers;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static void swap(int a, int b)
+    {
+        int num1 = a;
+        int num2 = b;
+        int temp =0;
+
+        temp = num2;
+        num2 = num1;
+        num1=temp;
+
+        Console.WriteLine("afrer swaping num 1: "+num1);
+         Console.Write("afrer swaping num 2: "+num2);
+
+    }
+    static void Main(){
+        int a = 12;
+        int b =32;
+
+        swap(a,b);
+    }
+}
