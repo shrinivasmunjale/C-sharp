75ae8eeb3e8e2aadba769186b0635c8903a0a3d9
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-02-04 11:27:27 +0530

    hello

---
+++ 8.complexInterest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class CompoundInterest
+{
+    static void Main()
+    {
+        double principal, rate, time, amount, compoundInterest;
+
+        Console.Write("Enter Principal Amount: ");
+        principal = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Rate of Interest (%): ");
+        rate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Time (in years): ");
+        time = Convert.ToDouble(Console.ReadLine());
+
+        amount = principal * Math.Pow((1 + rate / 100), time);
+        compoundInterest = amount - principal;
+
+        Console.WriteLine("\nTotal Amount = " + amount);
+        Console.WriteLine("Compound Interest = " + compoundInterest);
+
+       
+    }
+}
---
+++ 9.largestInThree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a,b,c;
+        Console.Write("Enter first number: ");
+        a =Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b =Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter thired number: ");
+        c =Convert.ToInt32(Console.ReadLine());
+
+        if ( a > b && a> c)
+        {
+            Console.WriteLine("Greater number is :"+ a);
+        }else if(b>a && b > c)
+        {
+             Console.WriteLine("Greater number is :"+ b);
+        }
+        else
+        {
+             Console.WriteLine("Greater number is :"+ c);
+        }
+
+
+
+
+
+    }
+}
